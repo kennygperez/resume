9bb240335eb92370369dfb8e97b8a786724c3f58
Kenny G Perez <kenny.perez@corsair.com>
2021-10-27 00:18:47 -0400

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import Document from './Document';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Document />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
